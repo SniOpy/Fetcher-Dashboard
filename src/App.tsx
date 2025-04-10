4f3777311466049ae86be17d717fe3869d67b1b4
SniOpy <soufianeibna@gmail.com>
2025-04-10 19:25:44 +0100

    ✨ F02 : APP > UserList > Add fakeUsers

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import UserList from './components/UserList';
 
 function App() {
-  return <h1>Fetcher Dashboard</h1>;
+  return (
+    <>
+      <h1>Fetcher Dashboard</h1>
+
+      <UserList />
+    </>
+  );
 }
 
 export default App;
